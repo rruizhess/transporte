d7bb04fa2a3ff38edcc9310bf777dfe73d4e4c4a
Juan <Juan@Juan-PC.aiep>
2015-05-02 19:57:57 -0300

    hola

---
+++ Transporte/src/Transporte/Transporte.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Transporte;
+
+/**
+ *
+ * @author Juan
+ */
+public class Transporte {
+int a();
+String nombres();
+double decimal();   
+
+}
